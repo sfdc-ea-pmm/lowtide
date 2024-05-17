0c1b36a7b41fe7a279dd115be0f214c8c184515d
Rodrigo Mercader <rmercader@salesforce.com>
2024-05-17 09:45:42 -0300

    Update session.js

--- src/auth/session.js
+++ src/auth/session.js
@@ -24,7 +24,7 @@ exports.store = (req) => {
     
     sf_object.opened_date = new Date()
 
-    const resx = conn.identity(async (err, _) => {
+    conn.identity(async (err, _) => {
       if (!err) {
 
         console.log(conn.userInfo);
@@ -50,11 +50,6 @@ exports.store = (req) => {
         reject("Could not authenticate with session information.")
       }
     })
-
-    console.log(`user ID: ${resx.user_id}`);
-    console.log(`organization ID: ${resx.organization_id}`);
-    console.log(`username: ${resx.username}`);
-    console.log(`display name: ${resx.display_name}`);
   })
 
 }
