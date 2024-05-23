a144f09ee8c27a8e6065987a3c007c38cc83f2a9
Rodrigo Mercader <rmercader@salesforce.com>
2024-05-23 11:01:16 -0300

    Getting user id info from chatter when SessionType is SubstituteUser

--- src/auth/session.js
+++ src/auth/session.js
@@ -18,65 +18,50 @@ exports.store = (req) => {
 
   return new Promise((resolve, reject) => {
 
-    console.log("auth_credentials:");
-    console.log(auth_credentials);
-
-    const conn = new jsforce.Connection(auth_credentials)
-    
-    sf_object.opened_date = new Date()
+    const conn = new jsforce.Connection(auth_credentials);   
+    sf_object.opened_date = new Date();
 
     conn.identity(async (err, res) => {
       if (!err) {
-        console.log("User Info:", conn.userInfo);
+        let userId = '';
 
         // Check if userInfo.id is present and valid
         if (conn.userInfo && conn.userInfo.id) {
-          try {
-            const relatedUser = await conn
-              .sobject("User")
-              .retrieve(conn.userInfo.id);
-
-            sf_object.opened_date = new Date();
-            sf_object.api = await getVersion(conn);
-
-            sf_object.auth_response = {
-              id: conn.userInfo.id,
-              name: relatedUser.Name,
-              username: relatedUser.Username,
-              accessToken: conn.accessToken,
-              instanceUrl: conn.instanceUrl,
-            };
-
-            resolve(sf_object);
-          } catch (retrieveErr) {
-            console.error("Error retrieving user:", retrieveErr);
-            reject("Error retrieving user: " + retrieveErr.message);
-          }
-        } else {          
+          userId = conn.userInfo.id;
+        } 
+        else {          
           //reject("Invalid user info received.");
-          console.error("Invalid user info received. Attempting to retrieve from Chatter API");
-          
+          console.log("Invalid user info received - maybe due to SubstituteUser session type. Attempting to retrieve user info from Chatter API...");          
           const chatterRes = await conn.chatter.resource('/users/me');
-          console.log(chatterRes);
-
-          const relatedUser = await conn
-              .sobject("User")
-              .retrieve(chatterRes.id);
-
-            sf_object.opened_date = new Date();
-            sf_object.api = await getVersion(conn);
-
-            sf_object.auth_response = {
-              id: conn.userInfo.id,
-              name: relatedUser.Name,
-              username: relatedUser.Username,
-              accessToken: conn.accessToken,
-              instanceUrl: conn.instanceUrl,
-            };
-
-            resolve(sf_object);
+          userId = chatterRes.id;
         }
-      } else {
+
+        // Now I should have a user id properly set
+
+        try {
+          const relatedUser = await conn
+            .sobject("User")
+            .retrieve(userId);
+
+          sf_object.opened_date = new Date();
+          sf_object.api = await getVersion(conn);
+
+          sf_object.auth_response = {
+            id: userId,
+            name: relatedUser.Name,
+            username: relatedUser.Username,
+            accessToken: conn.accessToken,
+            instanceUrl: conn.instanceUrl,
+          };
+
+          resolve(sf_object);
+        } 
+        catch (retrieveErr) {
+          console.error("Error retrieving user:", retrieveErr);
+          reject("Error retrieving user: " + retrieveErr.message);
+        }
+      }
+      else {
         console.error("Identity error:", err);
         reject("Could not authenticate with session information.");
       }
